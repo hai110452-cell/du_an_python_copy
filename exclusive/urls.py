f2bd58f66a521888619f99b4ce2a0bcbe854d8e3
phuochoangbmt-svg <phuochoangbmt@gmail.com>
2026-04-15 08:49:00 +0700

    update database config

--- exclusive/urls.py
+++ exclusive/urls.py
@@ -1,4 +1,3 @@
-# exclusive/urls.py
 from django.urls import path
 from . import views
 from .views import ExclusiveItemUploadAPI
@@ -9,6 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('all/', views.all_items, name='all'),
     path('<int:pk>/', views.exclusive_detail, name='detail'),
+
     path('download/<str:type>/<int:id>/', views.add_download, name='add_download'),
     path('favorite/<str:type>/<int:id>/', views.add_favorite, name='add_favorite'),
     path('remove-download/<str:type>/<int:id>/', views.remove_download, name='remove_download'),
