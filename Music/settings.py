6fe5f5906ce7a457316badcc430023fe1a5dae83
phuochoangbmt-svg <phuochoangbmt@gmail.com>
2026-04-15 07:13:18 +0700

    update database config

--- Music/settings.py
+++ Music/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-@q197fcbk1nb=ub2=hy79xgenzbs+btxiut2wyq1u(r(lgi0)b'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # Để dấu * để dễ dàng chạy trên mọi server lúc mới deploy
 
